967d2013c72bb027436dccbd9f584e350290ce8c
Daniel Pachon <danset01@gmail.com>
2024-10-21 20:43:50 -0500

    actualizacion interfaz

--- app.py
+++ app.py
@@ -6,14 +6,89 @@ from streamlit_folium import st_folium
 from PIL import Image
 import requests
 from io import BytesIO
+import base64
+import locale
 
-import streamlit as st
+# Establecer el locale (en este caso, 'es_CO' para Colombia)
+locale.setlocale(locale.LC_ALL, 'es_CO.UTF-8')
+
+st.set_page_config(layout="wide")
+
 import requests
-from PIL import Image
-from io import BytesIO
+
+def obtener_puntos_interes(lat, lon, radio):
+    # Construir la consulta Overpass
+    query = f"""
+    [out:json];
+    (
+      node(around:{radio},{lat},{lon});
+      way(around:{radio},{lat},{lon});
+      relation(around:{radio},{lat},{lon});
+    );
+    out body;
+    """
+    
+    # Hacer la solicitud a la Overpass API
+    response = requests.get("http://overpass-api.de/api/interpreter", params={'data': query})
+    
+    if response.status_code == 200:
+        data = response.json()
+        lugares = []
+        for element in data['elements']:
+            if 'tags' in element:
+                # Extraer solo las etiquetas highway, amenity y shop
+                etiquetas = {k: v for k, v in element['tags'].items() if k in ['highway', 'amenity', 'shop']}
+                
+                if etiquetas:  # Solo agregar si tiene alguna de estas etiquetas
+                    # Obtener el nombre del lugar, si existe
+                    nombre = element['tags'].get('name', 'Sin nombre')  # Verificar si existe la etiqueta 'name'
+                    
+                    # Depuración: Mostrar el nombre del lugar
+                    print(f"Nombre del lugar: {nombre}")  # Depuración
+                    
+                    lugar = {
+                        'nombre': nombre,  # Usar el nombre que se encuentre
+                        'lat': element.get('lat'),
+                        'lon': element.get('lon'),
+                        'tags': etiquetas
+                    }
+                    lugares.append(lugar)
+        
+        return lugares
+        return data['elements']
+    else:
+        st.error("Error al obtener datos de la Overpass API.")
+        return None
+
+def mostrar_imagen_con_flechas(imagenes_urls, max_alto=400, max_ancho=600):
+    if len(imagenes_urls) > 0:
+        # Inicializar el índice de la imagen en st.session_state si no existe
+        if 'imagen_index' not in st.session_state:
+            st.session_state.imagen_index = 0
+        
+        # Contenedor para los botones de navegación
+        col1, col2 = st.columns([0.1, 0.1])
+
+        with col1:
+            if st.button("←", key=f"anterior_{index}", use_container_width=True):
+                # Navegar hacia la imagen anterior
+                if st.session_state.imagen_index > 0:
+                    st.session_state.imagen_index -= 1
+                # Forzar la actualización de la imagen mostrada
+
+        with col2:
+            if st.button("→", key=f"siguiente_{index}", use_container_width=True):
+                # Navegar hacia la imagen siguiente
+                if st.session_state.imagen_index < len(imagenes_urls) - 1:
+                    st.session_state.imagen_index += 1
+                # Forzar la actualización de la imagen mostrada
+        # Mostrar la imagen actual
+        mostrar_imagen_ajustada(imagenes_urls[st.session_state.imagen_index], max_alto, max_ancho)
+    else:
+        st.write("No hay imágenes disponibles para esta propiedad.")
 
 # Función para cargar la imagen desde URL y ajustar su tamaño si es necesario
-def mostrar_imagen_ajustada(imagen_url, max_alto, max_ancho=400):
+def mostrar_imagen_ajustada(imagen_url, max_alto, max_ancho=500):
     # Cargar la imagen desde la URL
     response = requests.get(imagen_url)
     img = Image.open(BytesIO(response.content))
@@ -41,19 +116,23 @@ def mostrar_imagen_ajustada(imagen_url, max_alto, max_ancho=400):
     img_bytes = BytesIO()
     img.save(img_bytes, format="PNG")
     img_bytes.seek(0)
+    img_base64 = base64.b64encode(img_bytes.read()).decode()
 
     # Mostrar la imagen centrada con HTML en Streamlit
     st.markdown(
         f"""
-        <div style="display: flex; justify-content: center;">
-            <img src="{imagen_url}" style="width: {nuevo_ancho}px; height: {nuevo_alto}px;">
+        <div style="display: flex; justify-content: center">
+            <img src="data:image/png;base64,{img_base64}" style="width: {nuevo_ancho}px; height: {nuevo_alto}px;">
+        </div>
+        
+        <div id="modal" style="display:none; position:fixed; z-index: 200; left: 0; top: 0; width: 100%; height: 100%; overflow: auto; background-color: rgba(0,0,0,0.9);">
+            <span onclick="document.getElementById('modal').style.display='none'" 
+                  style="color: white; float: right; font-size: 40px; font-weight: bold; cursor: pointer;">&times;</span>
+            <img id="modal-img" style="margin: auto; display: block; width: auto; max-width: 90%; max-height: 90%; margin-top: 5%;">
         </div>
         """, 
         unsafe_allow_html=True
-    )
-
-    
-st.set_page_config(layout="wide")
+    )  
 
 st.markdown("""
     <style>
@@ -81,28 +160,48 @@ st.write("Acá podrás encontrar las mejores ofertas para arrendar local, obtene
 # Parámetros de conexión a la base de datos en Railway
 @st.cache_resource
 def init_connection():
-    return psycopg2.connect(
-        host="autorack.proxy.rlwy.net",
-        user="postgres",  # Cambia esto según tu configuración
-        password="rJmaVfPPjUZARaPzwemDoMDvengICOrS",  # Cambia esto según tu configuración
-        dbname="railway",
-        port="33601"
-    )
+    try:
+        conn = psycopg2.connect(
+            #host="autorack.proxy.rlwy.net",
+            #user="postgres",  # Cambia esto según tu configuración
+            #password="rJmaVfPPjUZARaPzwemDoMDvengICOrS",  # Cambia esto según tu configuración
+            #dbname="railway",
+            #port="33601"
+            host="localhost",
+            user="postgres",  # Cambia esto según tu configuración
+            password="Danset01*",  # Cambia esto según tu configuración
+            dbname="hackaton",
+            port="5432"
+        )
+        return conn
+    except psycopg2.Error as e:
+        st.error(f"Error conectando a la base de datos: {e}")
+        return None
 
 conn = init_connection()
 
 # Función para ejecutar consultas y obtener datos
 @st.cache_data(ttl=600)  # Cachear por 10 minutos
 def obtener_datos(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        datos = cur.fetchall()
-        columnas = [desc[0] for desc in cur.description]
-        return pd.DataFrame(datos, columns=columnas)
+    try:
+        with conn.cursor() as cur:
+            cur.execute(query)
+            datos = cur.fetchall()
+            columnas = [desc[0] for desc in cur.description]
+            return pd.DataFrame(datos, columns=columnas)
+    except psycopg2.Error as e:
+        # Si ocurre un error en la transacción, se hace rollback
+        conn.rollback()  # Importante: deshacer la transacción fallida
+        st.error(f"Ocurrió un error al ejecutar la consulta: {e}")
+        return pd.DataFrame()  # Retornar un DataFrame vacío para evitar que falle el flujo
 
 # Consulta inicial para cargar todos los datos (incluyendo los ubicacion)
 consulta_default = "SELECT * FROM propiedades LIMIT 100;"  # Cambia el límite o quítalo según tu preferencia
 df = obtener_datos(consulta_default)
+
+def obtener_imagenes(propiedad_id):
+    query = f"SELECT url FROM imagenes WHERE propiedad_id={propiedad_id};"
+    return obtener_datos(query)["url"].tolist()
 
 df["lat"] = pd.to_numeric(df["lat"], errors="coerce")
 df["lon"] = pd.to_numeric(df["lon"], errors="coerce")
@@ -124,6 +223,18 @@ barrio_seleccionado = st.sidebar.selectbox("Selecciona una Ubicacion:", barrios)
 if barrio_seleccionado != "Seleccionar Ubicacion":
     # Filtrar los datos por el ubicacion seleccionado
     df_filtrado = df[df["barrio"] == barrio_seleccionado]
+    puntos_interes_lista = []
+    
+    # Reiniciar el índice de la imagen solo si es una nueva selección
+    if 'previous_selection' not in st.session_state or st.session_state.previous_selection != barrio_seleccionado:
+        st.session_state.imagen_index = 0  # Reinicia el índice al seleccionar una nueva ubicación
+        
+        # Reiniciar los estados de los expanders
+        st.session_state.expanders = {f"expander_{index}": False for index in range(len(df_filtrado))}
+        st.session_state.previous_selection = barrio_seleccionado  # Actualizar la selección anterior
+    
+    if 'max_image_width' not in st.session_state:
+            st.session_state.max_image_width = 400
     
     if 'expanders' not in st.session_state:
         st.session_state.expanders = {}
@@ -139,29 +250,14 @@ if barrio_seleccionado != "Seleccionar Ubicacion":
     # Verifica que las columnas existan en el DataFrame
     if "area_privada" in df_filtrado.columns and "precio" in df_filtrado.columns and "descripcion" in df_filtrado.columns and "estrato" in df_filtrado.columns and "img_src" in df_filtrado.columns and "admin_price" in df_filtrado.columns and "bathrooms" in df_filtrado.columns and "lat" in df_filtrado.columns and "lon" in df_filtrado.columns and "titulo" in df_filtrado.columns and "sector" in df_filtrado.columns and "contact" in df_filtrado.columns:
         #st.subheader(f"Datos del Inmueble: {barrio_seleccionado}")
-            
-        if 'max_image_width' not in st.session_state:
-            st.session_state.max_image_width = 400
-            
-            # Reiniciar el estado de los expanders
-        if 'previous_selection' not in st.session_state or st.session_state.previous_selection != barrio_seleccionado:
-            for index in range(len(df_filtrado)):
-                expander_key = f"expander_{index}"
-                st.session_state.expanders[expander_key] = False
-        st.session_state.previous_selection = barrio_seleccionado
 
         # Iterar sobre las tres columnas simultáneamente
-        for index, (area, precio, descripcion, estrato, imagen, lat, lon, admin_price, bathrooms, titulo, sector, contact) in enumerate(zip(df_filtrado["area_privada"], df_filtrado["precio"], df_filtrado["descripcion"], df_filtrado["estrato"], df_filtrado["img_src"], df_filtrado["lat"], df_filtrado["lon"], df_filtrado["admin_price"], df_filtrado["bathrooms"], df_filtrado["sector"], df_filtrado["titulo"], df_filtrado["contact"]), start=1):
-            #st.subheader(f"{titulo}")
-            #st.write(f"{sector}")
-            #st.write(f"  Área privada: {area}")
-            #st.write(f"  Precio: {precio} COP")
-            #st.write(f"  Estrato: {estrato}")
-            #if admin_price != '[null]':
-                #st.write(f"  Admin precio: {admin_price}")
-            #if bathrooms != '[null]':
-            #    st.write(f"  Baños: {bathrooms}")
-            
+        for index, (area, precio, descripcion, estrato, imagen, lat, lon, 
+                    admin_price, bathrooms, titulo, sector, contact, id_propiedad) in enumerate(zip(df_filtrado["area_privada"], df_filtrado["precio"], df_filtrado["descripcion"], df_filtrado["estrato"], 
+                                                                                                    df_filtrado["img_src"], df_filtrado["lat"], df_filtrado["lon"], df_filtrado["admin_price"], 
+                                                                                                    df_filtrado["bathrooms"], df_filtrado["sector"], df_filtrado["titulo"], df_filtrado["contact"],
+                                                                                                    df_filtrado["id"]), start=1):
+            imagenes_urls = obtener_imagenes(id_propiedad)
             cols = st.columns([1, 2, 1])
             
             with cols[0]:
@@ -169,16 +265,17 @@ if barrio_seleccionado != "Seleccionar Ubicacion":
                 
             with cols[1]:
                 st.subheader(f"{titulo}")
-                st.write(f"  Precio: {precio} COP")
-                st.write(f"  Estrato: {estrato}")
-                st.write(f"  Área privada: {area}")
+                #st.write(f"  Precio: {precio} COP")
+                st.markdown(f"<p style='font-size:21px;'><b>Precio: </b>{precio}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size:21px;'><b>Estrato: </b>{estrato}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size:21px;'><b>Area Privada: </b>{area}</p>", unsafe_allow_html=True)
                 if admin_price!= '[null]':
-                    st.write(f"  Admin precio: {admin_price}")
+                    formateado = locale.format_string("%.0f", int(admin_price), grouping=True)
+                    st.markdown(f"<p style='font-size:21px;'><b>Precio administracion: </b>${formateado} COP</p>", unsafe_allow_html=True)
                 if bathrooms!= '[null]':
-                    st.write(f"  Baños: {bathrooms}")
+                    st.markdown(f"<p style='font-size:21px;'><b>Baños: </b>{bathrooms}</p>", unsafe_allow_html=True)
             
             with cols[2]:
-                expander_key = f"expander_{index}"
                 expander_key = f"expander_{index}"
         
                 # Inicializar el estado del expander si no existe
@@ -186,28 +283,77 @@ if barrio_seleccionado != "Seleccionar Ubicacion":
                     st.session_state.expanders[expander_key] = False
 
                 # Botón para ver más detalles
-                if st.button(f"Ver detalles {index}"):
+                if st.button("Ver detalles", key=f"details_{index}"):
                     # Cambiar el estado del expander
                     st.session_state.expanders[expander_key] = not st.session_state.expanders[expander_key]
 
             # Mostrar el expander si el estado es True
             if st.session_state.expanders[expander_key]:
                 with st.expander(f"Detalles de {titulo}", expanded=True):
-                    st.write(f"**Contacto:** {contact}")
-                    if descripcion:
-                        st.write(f"**Descripción:** {descripcion}")
+                    st.markdown(f"<p style='font-size:21px;'><b>Contacto:</b> {contact}</p>", unsafe_allow_html=True)
                     
-                    cols = st.columns([1, 2])
+                    if descripcion:
+                        st.markdown(f"<p style='font-size:18px;'><b>Descripción:</b> {descripcion}</p>", unsafe_allow_html=True)
+                        
+                    cols = st.columns([1, 1])
+                    
                     with cols[0]:
-                        mostrar_imagen_ajustada(imagen, st.session_state.max_image_width)
+                        mostrar_imagen_con_flechas(imagenes_urls)
 
                     with cols[1]:
+                        # Luego en tu parte donde muestras el mapa
                         if lat != 0 and lon != 0:
+                            # Definir el radio de búsqueda en metros
+                            radio_busqueda = 100  # Cambia esto según tus necesidades
+                            
+                            amenidades = obtener_puntos_interes(lat, lon, radio=radio_busqueda)
+                            
+                            # Crear el mapa
                             mapa = folium.Map(location=[lat, lon], zoom_start=16)
-                            folium.Marker([lat, lon], popup=f"Registro {index}: {descripcion}").add_to(mapa)
-                            st_folium(mapa, width=500, height=400)
-                        
-            st.markdown("---")                 
 
+                            # Agregar el marcador para la propiedad
+                            folium.Marker([lat, lon], popup=f"Registro {index}: {titulo}", icon=folium.Icon(color='blue')).add_to(mapa)
+
+                            # Procesar los resultados y extraer las etiquetas clave-valor solo para highway, amenity, y shop
+                            for lugar in amenidades:
+                                nombre = lugar.get('nombre', 'Sin nombre')
+                                lat_punto = lugar.get('lat')
+                                lon_punto = lugar.get('lon')
+                                
+                                # Extraer etiquetas clave-valor solo para 'highway', 'amenity', y 'shop'
+                                etiquetas = lugar.get('tags', {})
+                                
+                                # Asignar un color basado en la etiqueta
+                                if 'amenity' in etiquetas:
+                                    color = 'green'  # Color para amenity
+                                elif 'highway' in etiquetas:
+                                    color = 'gray'  # Color para highway
+                                elif 'shop' in etiquetas:
+                                    color = 'orange'  # Color para shop
+                                else:
+                                    color = 'gray'  # Color predeterminado para otros casos
+                                
+                                # Agregar a la lista de puntos de interés solo las etiquetas relevantes
+                                for clave, valor in etiquetas.items():
+                                    puntos_interes_lista.append([nombre, lat_punto, lon_punto, clave, valor])
+                                    
+                                    if lat_punto and lon_punto:
+                                        # Agregar un marcador para cada centro comercial
+                                        folium.Marker([lat_punto, lon_punto], popup=nombre, icon=folium.Icon(color=color)).add_to(mapa)
+
+                            # Mostrar el mapa en Streamlit
+                            st_folium(mapa, width=500, height=400)
+
+                    # Convertir la lista a un DataFrame para visualización
+                    df_puntos_interes = pd.DataFrame(puntos_interes_lista, columns=['Nombre', 'Latitud', 'Longitud', 'Etiqueta', 'Valor'])
+                    
+                    excluir_etiquetas = ['footway', 'service', 'crossing', 'cycleway', 'tertiary']
+                    df_filtrado = df_puntos_interes[~df_puntos_interes['Valor'].isin(excluir_etiquetas)]
+
+                # Mostrar la tabla en Streamlit
+                with st.expander("Ver puntos de interés cercanos"):
+                    st.write("Aquí tienes la lista de puntos de interés:")
+                    st.table(df_filtrado)  
+            st.markdown("---")            
 else:
     st.info("Selecciona un barrio para ver los datos correspondientes.")
